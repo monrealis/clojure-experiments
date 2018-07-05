b428321a50aea6e600e0cc08bb4713666a079c18
Vytenis Bivainis <monrealis@gmail.com>
2018-07-06 00:01:00 +0300

    Add main to hello world

--- hello-world/project.clj
+++ hello-world/project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main hello-world.core
   :dependencies [[org.clojure/clojure "1.7.0"]])
